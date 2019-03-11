181ad17bf978fe87b7d289140c565b847457dd18
Burak Kocak <burakkocak884@ghotmail.com>
2019-03-11 15:17:01 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,70 @@
 require 'open-uri'
 require 'pry'
+# require_relative './student.rb'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
-  end
+	  def self.scrape_index_page(index_url)
+	   	info_list = []
+	 
+	  	 html = open(index_url)
+	  	 document = Nokogiri::HTML(html)
+	  	student = document.css(".student-card")
+	  	#binding.pry
+	  	student.each do |each_one|
+	  		students = {}
 
-  def self.scrape_profile_page(profile_url)
-    
-  end
+			
+	  		students[:name] = each_one.css("h4").text
+	  		students[:location] = each_one.css("p").text
+	  		students[:profile_url] = each_one.css("a").first["href"]
+	  		#binding.pry
+	  		info_list << students
+	  			
+	  		#binding.pry
+	  	end
+		info_list
+	  	#binding.pry
+	 end
 
-end
+	  def self.scrape_profile_page(profile_url)
+	  	#binding.pry
+	     
+	   		 students = {}
+	  	
+
+			html = open(profile_url)
+		  	document = Nokogiri::HTML(html)
+		  	student = document.css(".social-icon-container a")
+		  	student_bio = document.css(".details-container")
+		  	quote = document.css(".vitals-text-container")
+		  	 
+            #binding.pry
+		  	student.each do |each_one|
+	  		
+            #binding.pry
+	  		if each_one.attribute("href").value.include?("twitter")
+	  						students[:twitter] = each_one.attribute("href").value
+	  				elsif each_one.attribute("href").value.include?("linkedin")
+	  						students[:linkedin] = each_one.attribute("href").value 
+	  			 	elsif each_one.attribute("href").value.include?("github")
+	  			 			students[:github] = each_one.attribute("href").value
+	  			 	else
+	  	
+	  				students[:blog] = each_one.attribute("href").value 
+	  		end
+	  	end
+	  	
+	  		
+	  		    students[:profile_quote] = quote.css(".profile-quote").text 
+	  		    students[:bio] = student_bio.css("p").text
+	  		 
+	  		 	students
+	   
+	   #binding.pry
+
+	  	end
+
+	
+	end
 
--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -6,17 +7,36 @@ class Student
 
   def initialize(student_hash)
     
+    #binding.pry
+   
+    student_hash.each do |attributes, its_value|
+      self.send(("#{attributes}="),its_value)
+    end
+   
+
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
+    #binding.pry
+      students_array.each do |students|
+        #binding.pry
+        student = self.new(students)
+      end
     
   end
 
   def add_student_attributes(attributes_hash)
+    #binding.pry
+    attributes_hash.each do |attributes, its_value|
+      self.send(("#{attributes}="),its_value)
+      #binding.pry
+    end
     
   end
 
   def self.all
+    @@all
     
   end
 end
